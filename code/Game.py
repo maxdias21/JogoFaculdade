ef4dee5faf0747453114a042ed1680b5b434b431
Max <maxdias.vp@gmail.com>
2025-10-02 23:19:48 -0300

    Completed Game

--- code/Game.py
+++ code/Game.py
@@ -4,6 +4,7 @@ from code.Menu import Menu
 
 from .Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 from .Level import Level
+from .Score import Score
 
 
 class Game:
@@ -13,6 +14,7 @@ class Game:
 
     def run(self, ):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -24,6 +26,12 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'level2', menu_return, player_score)
                     level_return = level.run(player_score)
+
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
